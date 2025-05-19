49d393e77a43a628ebb268f00db3bc646b1fbbdf
MoonFilmsGP <josegilpativilla2000@gmail.com>
2025-05-18 22:24:10 -0600

    qfix doble html

--- js/articles-loader.js
+++ js/articles-loader.js
@@ -11,7 +11,7 @@ fetch('index.json')
         <img src="${article.thumbnail}" alt="${article.title}" />
         <h2>${article.title}</h2>
         <p>${article.lead}</p>
-        <a href="articles/${article.path}>Leer más</a>
+        <a href="articles/${article.path}">Leer más</a>
       `;
             container.appendChild(card);
         });
